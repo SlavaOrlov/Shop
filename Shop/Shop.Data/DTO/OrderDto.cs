39bdde6799355135082f8a51947ee328854a46bd
SlavaOrlov <62886375+SlavaOrlov@users.noreply.github.com>
2020-08-19 10:01:34 +0300

    Add files via upload

---
+++ Shop/Shop.Data/DTO/OrderDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shop.Data.DTO
+{
+    public class OrderDto
+    {
+        public int Id { get; set; }
+        public string Fio { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ Shop/Shop.Data/DTO/OrderProductDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shop.Data.DTO
+{
+    public class OrderProductDto
+    {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
